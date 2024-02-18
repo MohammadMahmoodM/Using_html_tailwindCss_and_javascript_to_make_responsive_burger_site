a7f086b281c87c78eb06af4d5c4be4c9d4af2b3f
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2024-02-18 12:39:36 +0400

    nav opening closing functionality though javascript is completed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 const menuDiv = document.getElementById('menuicon')
 const navBarDiv = document.getElementById('navLinks')
+const closeIconDiv = document.getElementById('crossIcon')
 
 menuDiv.addEventListener("click", ()=>{
     return(
         navBarDiv.classList.remove('hidden')
+    )
+})
+
+closeIconDiv.addEventListener('click', () => {
+    return(
+        navBarDiv.classList.add('hidden')
     )
 })
